6906de6b0d0189896fab899043a3a36ed10d907e
hassanmasood483 <hassanshykh483@gmail.com>
2024-07-27 02:04:17 -0700

    updated

--- Branch.js
+++ Branch.js
@@ -1,9 +1,9 @@
 func1 = () => {
   console.log("this is function1");
 };
-func5 = () => {
-  console.log("this is function5");
+func2 = () => {
+  console.log("this is function2");
 };
-func4 = () => {
-  console.log("this is function4");
+func3 = () => {
+  console.log("this is function3");
 };
