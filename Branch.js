a7ab9866eab692deae1c9b13920934704dfc77ce
hassanmasood483 <hassanshykh483@gmail.com>
2024-07-27 15:22:11 -0700

    variables

--- Branch.js
+++
@@ -1,9 +0,0 @@
-func1 = () => {
-  console.log("this is function1");
-};
-func4 = () => {
-  console.log("this is function4");
-};
-func5 = () => {
-  console.log("this is function5");
-};
---
+++ variables.js
@@ -0,0 +1,38 @@
+//Const
+
+// const a = "before scope";
+
+// console.log(a);
+
+// {
+//   const a = "within scope";
+//   console.log(a);
+// }
+
+// console.log(a);
+
+// Let
+
+// let a = "before scope";
+
+// console.log(a);
+
+// {
+//   let a = "within scope";
+//   console.log(a);
+// }
+
+// console.log(a);
+
+//Var
+
+var a = "before scope";
+
+console.log(a);
+
+{
+  var a = "within scope";
+  console.log(a);
+}
+
+console.log(a);
