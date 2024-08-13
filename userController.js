a25bd5bd17f89f2d7726dc8f95206b1d54e2c18f
hassanmasood483 <hassanshykh483@gmail.com>
2024-08-12 21:54:03 -0700

    made a user controller and exported it

---
+++ userController.js
@@ -0,0 +1,32 @@
+// module.exports = {
+//   createUser: () => {
+//     console.log("created user function");
+//   },
+
+//   updateUser: () => {
+//     console.log("user updated");
+//   },
+//   getUser: () => {
+//     console.log("get user function->");
+//   },
+//   deleteUser: () => {
+//     console.log("user deleted");
+//   },
+// };
+
+userController = {
+  createUser: () => {
+    console.log("created user function");
+  },
+
+  updateUser: () => {
+    console.log("user updated");
+  },
+  getUser: () => {
+    console.log("get user function->");
+  },
+  deleteUser: () => {
+    console.log("user deleted");
+  },
+};
+module.exports = userController;
